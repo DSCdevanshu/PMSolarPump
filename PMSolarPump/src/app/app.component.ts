672cc0f28a21cbd73859064825f2b199b6afe975
User <amt9105@gmail.com>
2025-03-26 01:01:30 +0530

    added components

--- PMSolarPump/src/app/app.component.ts
+++ PMSolarPump/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { PhotoGalaryComponent } from "./photo-galary/photo-galary.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, PhotoGalaryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
