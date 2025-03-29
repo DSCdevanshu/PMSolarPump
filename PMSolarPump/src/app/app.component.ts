471231342cd3dc7e0994f723e9a40e65363c369b
User <amt9105@gmail.com>
2025-03-29 15:08:10 +0530

    Added Components

--- PMSolarPump/src/app/app.component.ts
+++ PMSolarPump/src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { PhotoGalaryComponent } from "./photo-galary/photo-galary.component";
+import { FormbtnComponent } from './formbtn/formbtn.component'; // Import the component
+import { YojnaDetComponent } from './yojna-det/yojna-det.component';
+import { FooterComponent } from "./footer/footer.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, PhotoGalaryComponent],
+  imports: [HomeComponent, PhotoGalaryComponent, FormbtnComponent, YojnaDetComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ PMSolarPump/src/app/formbtn/formbtn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formbtn',
+  standalone: true,
+  imports: [],
+  templateUrl: './formbtn.component.html',
+  styleUrl: './formbtn.component.css'
+})
+export class FormbtnComponent {
+
+}
