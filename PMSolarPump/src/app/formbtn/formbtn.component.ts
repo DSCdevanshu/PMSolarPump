a21515e6a59ad9d93c7714160f70c7308c20ce07
Devanshu <anshusingh34718@gmail.com>
2025-03-29 23:02:39 +0530

    modified:   PMSolarPump/angular.json 	modified:   PMSolarPump/src/app/formbtn/formbtn.component.html 	modified:   PMSolarPump/src/app/formbtn/formbtn.component.ts

--- PMSolarPump/src/app/formbtn/formbtn.component.ts
+++ PMSolarPump/src/app/formbtn/formbtn.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-formbtn',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './formbtn.component.html',
   styleUrl: './formbtn.component.css'
 })
