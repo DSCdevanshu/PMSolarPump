73ea80760e70fe2c06e00b327e7007268810b56b
Devanshu <anshusingh34718@gmail.com>
2025-03-30 11:35:05 +0530

    Final Page named FundingDepartment

--- PMSolarPump/src/app/form/form.component.ts
+++ PMSolarPump/src/app/form/form.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
 
+  constructor(private router:Router) {
+
+  }
+
+  onSubmit():void {
+    console.log("Submitted");
+    this.router.navigate(['/fundingDepartment']);
+  }
 }
